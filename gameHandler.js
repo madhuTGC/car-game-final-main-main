48751bd814787ce6b66ed2cb5747cb50b938e9d7
madhuTGC <madhu@thegamificationcompany.com>
2023-12-22 23:52:21 +0530

    +2

--- gameHandler.js
+++ gameHandler.js
@@ -1018,7 +1018,7 @@ document.addEventListener("touchstart", function (e) {
 
 document.addEventListener("touchmove", function (e) {
     e.preventDefault();
-    if (touchStartX !== undefined && raceCarStartX !== undefined && player.x < road.width)  {
+    if (touchStartX !== undefined && raceCarStartX !== undefined && e.touches[0].clientX>raceCarStartX)  {
         const touchX = e.touches[0].clientX;
         const deltaX = touchX - touchStartX;
         player.x = raceCarStartX + deltaX;
